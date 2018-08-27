f95b60b2e7fd127e91312a95b277e064ba2b4edc
Danihcv <dhcv@ic.ufal.br>
2018-08-26 23:31:06 -0300

    Encadeamento para frente implementado

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 const regexKnowledge = /[A-Za-z][A-Za-z0-9]*\s*:\s*(true|false)(\s*,\s*[A-Za-z][A-Za-z0-9]*:\s*(true|false))*\s*/;
 const regexRules = /!*[A-Za-z][A-Za-z0-9]*(&&!*[A-Za-z][A-Za-z0-9]*)*=>!*[A-Za-z][A-Za-z0-9]*(&&!*[A-Za-z][A-Za-z0-9]*)*(,!*[A-Za-z][A-Za-z0-9]*(&&!*[A-Za-z][A-Za-z0-9]*)*=>!*[A-Za-z][A-Za-z0-9]*(&&!*[A-Za-z][A-Za-z0-9]*)*)*/;
-const regexMemberElements = /(&&)|([A-Za-z][A-Za-z0-9]*)/g;
+const regexMemberElements = /(&&)|(!*[A-Za-z][A-Za-z0-9]*)/g;
 
 let knowledgeBase = {};
 let rulesBase = [];
@@ -13,25 +13,8 @@ class Rule {
      * @param consequent deve ser uma Tree/string
      */
     constructor(antecedent, consequent) {
-        this.antecedent = new Member(antecedent);
-        this.consequent = new Member(consequent);
-    }
-}
-
-class Member {
-
-    /**
-     * @param value deve ser uma Tree/string
-     */
-    constructor(value) {
-        this.value = value;
-    }
-
-    has(term, root = this.value) {
-        if (typeof root === 'string') {
-            return root === term;
-        }
-        return this.has(term, root.a) || this.has(term, root.b);
+        this.antecedent = antecedent;
+        this.consequent = consequent;
     }
 }
 
@@ -48,8 +31,8 @@ function initialize() {
     rulesBase = [];
     answerElement = document.getElementById('answer');
 
-    let inputKnowledge = document.getElementById('knowledge').value.toLowerCase().replace(/\s/g, '');
-    let inputRules = document.getElementById('rules').value.toLowerCase().replace(/\s/g, '');
+    let inputKnowledge = document.getElementById('knowledge').value.replace(/\s/g, '');
+    let inputRules = document.getElementById('rules').value.replace(/\s/g, '');
 
     if (!isValid(inputKnowledge, regexKnowledge)) {
         return alert('Base de conhecimento inválida');
@@ -77,6 +60,15 @@ function initialize() {
     }
 
     const question = document.getElementById('question').value;
+
+    if (document.getElementById('forward').checked) {
+        console.log('FORWARD METHOD');
+        answerElement.value = forwardSolution(question);
+    } else if (document.getElementById('backward').checked) {
+        console.log('BACKWARD METHOD');
+    } else {
+        console.log('HYBRID METHOD');
+    }
 }
 
 function isValid(str, regex, acceptEmpty = true) {
@@ -89,4 +81,52 @@ function generateTree(elements) {
     }
 
     return new Node(elements[0], generateTree(elements.slice(2)), elements[1]);
+}
+
+function forwardSolution(question) {
+    let prevKnowledgeSize = -1;
+    do {
+        prevKnowledgeSize = Object.keys(knowledgeBase).length;
+
+        for (let i = 0; i < rulesBase.length; i++) {
+            console.log('---');
+            if (solveTree(rulesBase[i].antecedent)) {
+                if (!addToKnowledgeBase(rulesBase[i].consequent)) {
+                    return 'Contradição';
+                }
+                console.log('currKB', knowledgeBase);
+            }
+        }
+    } while (prevKnowledgeSize !== Object.keys(knowledgeBase).length);
+
+    return knowledgeBase[question] === undefined ? 'Sem solução' : knowledgeBase[question];
+}
+
+function solveTree(root) {
+    console.log('solve', root);
+    if (typeof root === 'string') {
+        const baseValue = knowledgeBase[root.replace(/!/g, '')];
+        return baseValue !== undefined && (booleanValue(root) ? baseValue : !baseValue);
+    }
+    return solveTree(root.a) && solveTree(root.b);
+}
+
+function addToKnowledgeBase(root) {
+    console.log('add', root);
+    if (typeof root === 'string') {
+        if (knowledgeBase[root.replace(/!/g, '')] !== undefined) {
+            return knowledgeBase[root.replace(/!/g, '')] === booleanValue(root);
+        }
+        knowledgeBase[root.replace(/!/g, '')] = booleanValue(root);
+    } else {
+        return addToKnowledgeBase(root.a) && addToKnowledgeBase(root.b);
+    }
+    return true;
+}
+
+function booleanValue(term) {
+    if (term[0] === '!') {
+        return !booleanValue(term.substr(1));
+    }
+    return true;
 }
